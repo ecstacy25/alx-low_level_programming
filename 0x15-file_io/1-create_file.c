7a82ed0b65102446376b66e3682941981f29ac2d
ecstacy25 <ibiamjnr9@gmail.com>
2023-04-09 00:51:25 -0700

    A program that Creates a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (!text_content)
 		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
